b9d6325327890aab120cd0f6d980992fc95f1edd
Jaime Geiser <jaime.geiser@gmail.com>
2012-07-17 21:45:32 -0400

    un delete

--- test/login_php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-// Borramos toda la sesion
-session_destroy();
-echo 'Ha terminado la session <p><a href="index.php">index</a></p>';
-?>
-<SCRIPT LANGUAGE="javascript">
-location.href = "index.php";
-</SCRIPT>
